4fa229bd4388e810d53bee0816342aac4a389bde
yahir <yahirruizmen0@gmail.com>
2019-05-23 08:47:51 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,32 @@
-//JavaScript
- 
-$(document).ready(function(e){
-document.addEventListener("deviceready",function(){
-	
-	$('#izquierda').on("swipeleft",function(){
-		navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion","Aceptar"});
-	});
-	$('#derecha').on("swiperight",function(){
-		navigator .notification. confirm ("¿que quieres hacer?",function(opt){
-			switch(opt)
-			{
-				case 1:
-				navigator.notification.beep(1);
-				break;
-				
-				case 2:
-				navigator.notification.vibrate(1000);
-				break;
-			}
-		},"Aplicacion ","Beep,Vibrar,Cancelar");
-	});
-},false);
+//JavaScript Document
+
+$(document).ready(function (e) {
+	document.addEventListener ("deviceready", onDeviceReady, false);
+		
 });
 	
+	function onDeviceReady() {
 		
+		$('#posicion').on ('click', function(){
+			getPosition ();
+		});
+} 
+
+function getPosition(){
+	var options = {
+		enableHighAccuracy : true,
+		maximumAge: 3600000
+	}
+	
+	var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+	
+	function onSuccess(position){
+		
+		alert('Latitude:'    + position.coords.latitude    +'\n'+
+			'Longitude:'    + position.coords.longitude  +'\n');
+	};
+	
+	function onError(error){
+		alert('code: '  + error.code	+'\n'+ 'message: '+ error.message +'\n');
+	}
+}
